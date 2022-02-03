94d5aafe36ab3a9222fbf7aa41be80a6d9481a28
tetynurazizah19 <tetynurazizah618@gmail.com>
2022-02-02 20:54:39 -0800

    Initial commit

---
+++ app/src/main/java/com/example/profilsekolah/Latihan.java
@@ -0,0 +1,13 @@
+package com.example.profilsekolah;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Latihan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_latihan);
+    }
+}
